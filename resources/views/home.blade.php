9bb940ed76553876924b9bf0d1bf4274d696f486
Gianluca Benassi <benox1199@gmail.com>
2022-02-04 15:08:05 +0100

    movie cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,21 @@
 @endsection
 
 @section('pageContent')
-    @dump($movies)
+    <div class="home">
+        <div class="container">
+            <div class="row">
+
+                @foreach ($movies as $movie)
+                    <div class="card">
+                        <h2>{{$movie['title']}}</h2>
+                        <h3>Original title: {{$movie['original_title']}}</h3>
+                        <span><strong>Nationality:</strong> {{$movie['nationality']}}</span>
+                        <span><strong>Date:</strong> {{$movie['date']}}</span>
+                        <span><strong>Vote:</strong> {{$movie['vote']}}</span>
+                    </div>
+                @endforeach
+
+            </div>
+        </div>
+    </div>
 @endsection
