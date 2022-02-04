69b1b5eb120383da8ae070ea8f92617c7e5b681f
Gianluca Benassi <benox1199@gmail.com>
2022-02-04 14:30:39 +0100

    base layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Home page</h1>
-    @dd($movies)
-</body>
-</html>
+@extends('layouts.base')
+
+@section('pageTitle')
+    Home
+@endsection
+
+@section('pageContent')
+    @dump($movies)
+@endsection
