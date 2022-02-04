d92315e797149f6d7689867e10b4d12391a6f017
Gianluca Benassi <benox1199@gmail.com>
2022-02-04 14:47:37 +0100

    header style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,16 @@
 <header>
-    HEADER
+    <div class="container">
+        <div class="row">
+
+            <h1>MyMovies</h1>
+            
+            <nav>
+                <ul class="row">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="#">Movies complete list</a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </div>
 </header>
